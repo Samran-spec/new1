06ca3d4d1590097af0576a4ee6a8c0deed7f5ea9
Samran-spec <58138217+Samran-spec@users.noreply.github.com>
2020-07-24 11:03:47 -0700

    updated myapp

---
+++ myapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Dinner from './dinner.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Dinner dishName="chicken karahi"/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/dinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Dinner(props){
+  return  (
+        <div>
+            <h1> have a good dinner of  {props.dishName} </h1>
+        </div>
+    );
+        
+}
+export default Dinner;
